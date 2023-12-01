97f279b33ed7405274826cdd4fe5672ec7b9dfe0
erikanmann <erikanmann@gmail.com>
2023-12-01 16:12:37 +0200

    hover

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 function button(pageName){
     window.location.href = pageName
 }
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    var infoBox = document.getElementById("info-box");
+    var infoText = document.getElementById("info-text");
+    var locationMarkers = document.querySelectorAll(".location-marker");
+
+    locationMarkers.forEach(function(marker) {
+        marker.addEventListener("mouseover", function() {
+
+            infoBox.style.display = "block";
+
+            var isTopHalf = marker.offsetTop < window.innerHeight / 2;
+
+            if (isTopHalf) {
+                infoBox.style.top = marker.offsetTop + marker.offsetHeight + 10 + "px";
+            } else {
+                infoBox.style.top = marker.offsetTop - infoBox.offsetHeight - 10 + "px";
+            }
+
+            infoBox.style.left = marker.offsetLeft - infoBox.offsetWidth / 2 + marker.offsetWidth / 2 + "px";
+
+            infoText.textContent = marker.getAttribute("data-info");
+        });
+
+        marker.addEventListener("mouseout", function() {
+            infoBox.style.display = "none";
+        });
+    });
+});
